57bf51c3a75c894cc359b05017682a1aa40c9540
Gabriel Marques <bielmarques922@gmail.com>
2022-11-11 19:39:35 -0300

    PHP Exceptions - Modulo 05 - Sacando sem saldo

---
+++ 05-aplicando-o-conhecimento/src/Modelo/Conta/Conta.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Alura\Banco\Modelo\Conta;
+
+abstract class Conta
+{
+    private string $titular;
+    protected float $saldo;
+    private static int $numeroDeContas = 0;
+
+    /*
+     * $tipo 1 == Conta Corrente; 2 == Conta Poupança
+     */
+
+    public function __construct(Titular $titular)
+    {
+        $this->titular = $titular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        if (self::$numeroDeContas > 2) {
+            echo "Há mais de uma conta ativa";
+        }
+    }
+
+    public function saca(float $valorASacar): void
+    {
+        $tarifaDeSaque = $valorASacar * $this->percentualTarifa();
+        $valorSaque = $valorASacar + $tarifaDeSaque;
+        if ($valorSaque > $this->saldo) {
+            throw new SaldoInsuficienteException();
+        }
+
+        $this->saldo -= $valorSaque;
+    }
+
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->titular->recuperaNome();
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->titular->recuperaCpf();
+    }
+
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
+
+    abstract protected function percentualTarifa(): float;
+}
---
+++ 05-aplicando-o-conhecimento/src/Modelo/Funcionario/Funcionario.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alura\Banco\Modelo\Funcionario;
+
+use Alura\Banco\Modelo\{CPF, Pessoa};
+
+abstract class Funcionario extends Pessoa
+{
+    private float $salario;
+
+    public function __construct(string $nome, CPF $cpf, float $salario)
+    {
+        parent::__construct($nome, $cpf);
+        $this->salario = $salario;
+    }
+
+    public function recebeAumento($valorAumento): void
+    {
+        if($valorAumento < 0) {
+            echo "Aumento deve ser positivo";
+            return;
+        }
+
+        $this->salario += $valorAumento;
+    }
+
+    public function recuperaSalario(): float
+    {
+        return $this->salario;
+    }
+
+    abstract public function calculaBonificacao(): float;
+}
