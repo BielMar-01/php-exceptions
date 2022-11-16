03c59dfc61eed884f3d281269d6ebdb2173aeb11
Gabriel Marques <bielmarques922@gmail.com>
2022-11-15 21:06:48 -0300

    PHP Exceptions - Modulo 06 - Configuração de erros

---
+++ 06-finally-e-peculiaridades-do-php/02-teste.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', 'log/teste');
+
+echo $variavel;
+echo $array[12];
+
+echo CONSTANTE;
